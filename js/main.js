38672fdfa3c9478ae73aeb06aab3fd3964da0f5a
Den89 <malyhindenis8@gmail.com>
2022-12-08 14:07:24 +0500

    add slader

--- js/main.js
+++ js/main.js
@@ -68,3 +68,73 @@ $(function(){
 		animatedBlocks();
 	});
 })(jQuery);
+
+// =====================================
+
+var carousel = document.querySelector('.page3__columns');
+var firstImg = carousel.querySelectorAll('.page3_item')[0];
+var arr1 = document.querySelectorAll('.arr1');
+
+var isDragStar = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
+var firstImgWidth = firstImg.clientWidth + 14;
+var scrollWidth = carousel.scrollWidth - carousel.clientWidth;
+
+
+var showHideIcons = () => {
+   arr1[0].style.display = carousel.scrollLeft == 0 ? 'none' : 'block';
+   arr1[1].style.display = carousel.scrollLeft == scrollWidth ? 'none' : 'block';
+}
+
+arr1.forEach(icon =>{
+   icon.addEventListener('click', () => { // скрол стрелкой
+      carousel.scrollLeft += icon.id == 'left' ? -firstImgWidth : firstImgWidth;
+      setTimeout(() => showHideIcons(), 60);
+   });
+});
+
+var autoSlide = () => {
+   if(carousel.scrollLeft == (carousel.scrollWidth - carousel.clientWidth)) return;
+
+   positionDiff = Math.abs(positionDiff);
+   var firstImgWidth = firstImg.clientWidth + 14;
+   var valDiference = firstImgWidth - positionDiff;
+
+   if(carousel.scrollLeft > prevScrollLeft){
+      return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDiference : -positionDiff;
+   }
+   return carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDiference : -positionDiff;
+};
+
+var dragStar = (e) => {
+   isDragStar = true; // скрол картинкой
+   prevPageX = e.pageX || e.touches[0].pageX;
+   prevScrollLeft = carousel.scrollLeft;
+}
+
+var dragging = (e) => {
+   if(!isDragStar) return;
+   e.preventDefault();
+   isDragging = true;
+   carousel.classList.add('dragging');
+   positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+   carousel.scrollLeft = prevScrollLeft - positionDiff;
+   showHideIcons();
+}
+
+var dragStop = () => {
+   isDragStar = false;
+   carousel.classList.remove('dragging');
+   if(!isDragging) return;
+   isDragging = false;
+   autoSlide();
+}
+
+carousel.addEventListener('mousedown', dragStar);
+carousel.addEventListener('touchstart', dragStar);
+
+carousel.addEventListener('mousemove', dragging);
+carousel.addEventListener('touchmove', dragging);
+
+carousel.addEventListener('mouseup', dragStop);
+carousel.addEventListener('mouseleave', dragStop);
+carousel.addEventListener('touchend', dragStop);
